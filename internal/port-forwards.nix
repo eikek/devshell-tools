f1d9ef3727f93b8ec8362d4cffcf72da7b1ff7a0
eikek <eikek@posteo.de>
2024-08-12 11:48:28 +0200

    Fix port types in internal port-forward cfg

--- internal/port-forwards.nix
+++ internal/port-forwards.nix
@@ -8,74 +8,79 @@ in {
   options = {
     port-forward = with lib; {
       ssh = mkOption {
-        type = types.int;
+        type = types.port;
         default = 10022;
         description = "The target port for ssh on the host";
       };
       dev-postgres = mkOption {
-        type = types.int;
+        type = types.port;
         default = 15432;
         description = "The target port for postgres on the host";
       };
       dev-pgweb = mkOption {
-        type = types.int;
+        type = types.port;
         default = 15433;
         description = "The target port for pgweb on the host";
       };
       dev-solr = mkOption {
-        type = types.int;
+        type = types.port;
         default = 18983;
         description = "The target port for solr on the host";
       };
       dev-smtp = mkOption {
-        type = types.int;
+        type = types.port;
         default = 10025;
         description = "The target port for smtp on the host";
       };
       dev-imap = mkOption {
-        type = types.int;
+        type = types.port;
         default = 10143;
         description = "The target port for imap on the host";
       };
       dev-webmail = mkOption {
-        type = types.int;
+        type = types.port;
         default = 8080;
         description = "The target port for webmail on the host";
       };
       openapi-docs = mkOption {
-        type = types.int;
+        type = types.port;
         default = 8081;
         description = "The target port for openapi docs on the host";
       };
       dev-redis = mkOption {
-        type = types.int;
+        type = types.port;
         default = 16379;
         description = "The target port for redis on the host";
       };
       dev-mariadb = mkOption {
-        type = types.int;
+        type = types.port;
         default = 3330;
         description = "The target port for mariadb on the host";
       };
       dev-minio-api = mkOption {
-        type = types.int;
+        type = types.port;
         default = 9000;
         description = "The minio api port";
       };
       dev-minio-console = mkOption {
-        type = types.int;
+        type = types.port;
         default = 9001;
         description = "The minio console port";
       };
       dev-fuseki = mkOption {
-        type = types.int;
+        type = types.port;
         default = 3030;
         description = "The fuseki server port";
       };
       dev-keycloak = mkOption {
-        type = types.int;
+        type = types.port;
         default = 8180;
         description = "The keycloak web port";
+      };
+      dev-authentik = mkOption {
+        type = types.port;
+        default = 9010;
+        description = "The authentik web port";
       };
     };
   };
@@ -151,6 +156,11 @@ in {
         from = "host";
         host.port = cfg.dev-keycloak;
         guest.port = config.services.dev-keycloak.http-port;
+      })
+      (mkIf config.services.dev-authentik.enable {
+        from = "host";
+        host.port = cfg.dev-authentik;
+        guest.port = config.services.dev-authentik.port;
       })
     ];
   };

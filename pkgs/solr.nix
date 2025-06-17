1db1256aba5f93e13f519c80bcbc51368a358491
eikek <eikek@posteo.de>
2025-06-17 11:06:34 +0200

    Update solr

--- pkgs/solr.nix
+++ pkgs/solr.nix
@@ -8,11 +8,11 @@
 # https://discourse.nixos.org/t/solr-has-been-removed-what-are-my-options/33504/3
 stdenv.mkDerivation rec {
   pname = "solr";
-  version = "9.8.0";
+  version = "9.8.1";
 
   src = fetchurl {
     url = "mirror://apache/solr/${pname}/${version}/${pname}-${version}.tgz";
-    sha256 = "sha256-mUjc95jBlrg0xMu0INHqWZVHlDFmnSZsM9RlSLZ+aeE=";
+    sha256 = "sha256-p4kRATG8jLcbAjPVKOT6X/pWa6sFvHLygKjMknW9nj4=";
   };
 
   nativeBuildInputs = [makeWrapper];
@@ -31,7 +31,7 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with lib; {
-    homepage = "https://lucene.apache.org/solr/";
+    homepage = "https://solr.apache.org";
     description = "Open source enterprise search platform from the Apache Lucene project";
     license = licenses.asl20;
     latforms = platforms.all;

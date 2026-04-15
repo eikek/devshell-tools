6f5ba0ead2a272e1e39bc1b956e5cf973473375d
Eike <eikek@posteo.de>
2026-04-15 09:30:55 +0200

    update flake and solr

--- pkgs/solr.nix
+++ pkgs/solr.nix
@@ -8,11 +8,11 @@
 # https://discourse.nixos.org/t/solr-has-been-removed-what-are-my-options/33504/3
 stdenv.mkDerivation rec {
   pname = "solr";
-  version = "9.8.1";
+  version = "10.0.0";
 
   src = fetchurl {
     url = "mirror://apache/solr/${pname}/${version}/${pname}-${version}.tgz";
-    sha256 = "sha256-p4kRATG8jLcbAjPVKOT6X/pWa6sFvHLygKjMknW9nj4=";
+    sha256 = "sha256-B8GAlw9g0Td2vhPMtgxwfQQeXhqLkU0ZfRNYrCX4BLU=";
   };
 
   nativeBuildInputs = [makeWrapper];
@@ -21,13 +21,11 @@ stdenv.mkDerivation rec {
     mkdir -p $out
     cp -r * $out/
     rm -rf $out/bin/init.d
-    rm $out/bin/postlogs
     rm $out/bin/install_solr_service.sh
     rm $out/bin/solr.in.sh
     rm $out/bin/*.cmd
 
     wrapProgram $out/bin/solr --set JAVA_HOME "${jre}"
-    wrapProgram $out/bin/post --set JAVA_HOME "${jre}"
   '';
 
   meta = with lib; {
